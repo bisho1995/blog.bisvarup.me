4dcc1bca27415c6a792c381042dc41437cf74c8b
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-05-27 10:42:24 +0530

    added google analytics and meta description

--- src/component/Helmet/Helmet.tsx
+++ src/component/Helmet/Helmet.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import {Helmet} from 'react-helmet';
 
 export default function AppHelmet() {
   return (
@@ -7,6 +7,10 @@ export default function AppHelmet() {
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+      />
+      <meta
+        name="description"
+        content="Technical blog regarding bisvarup's views and experiences. Subscribe now to get the best industry standard information delivered to you."
       />
       <title>bisvarup&apos;s blog</title>
     </Helmet>

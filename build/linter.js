5431c5e8d7c2184c6d0369291d61615e1010ecc0
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-06-17 15:59:00 +0530

    .

--- build/linter.js
+++ build/linter.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const { EOL } = require('os');
-const { execSync } = require('child_process');
+const {EOL} = require('os');
+const {execSync} = require('child_process');
 const fs = require('fs-extra');
 const chalk = require('chalk');
 
@@ -21,7 +21,7 @@ try {
 
       const lintCode = () => {
         const val = execSync(
-          `./node_modules/.bin/eslint ${p} --ext=ts,tsx,js,jsx --fix`, { stdio: 'pipe' },
+          `./node_modules/.bin/eslint ${p} --ext=ts,tsx,js,jsx --fix`,
         );
 
         console.log(val.toString());
